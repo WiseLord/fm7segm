7aed71674b8106017aecbdb021334b4d154468be
WiseLord <wiselord1983@gmail.com>
2015-01-02 20:36:31 +0300

    Added tux032 to build script.

--- build_all.sh
+++ build_all.sh
@@ -4,7 +4,7 @@ for IND_TYPE in _CA _CC
 do
 	for USE_TRANS in _NO _TR
 	do
-		for TUNER in RDA5807 TEA5767
+		for TUNER in RDA5807 TEA5767 TUX032
 		do
 			make clean
 			make IND_TYPE=${IND_TYPE} USE_TRANS=${USE_TRANS} TUNER=${TUNER}
